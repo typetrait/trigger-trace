7546ecc26519ccee5292fa9bfe99aa79a89edd29
alchemistis <14222884+alchemistis@users.noreply.github.com>
2023-05-03 20:03:55 -0300

    refactor: minor cleanup

--- trigger_trace.lua
+++ trigger_trace.lua
@@ -73,7 +73,6 @@ local function draw_wireframe_box(lower_corner_pos, upper_corner_pos, color)
             draw.line(back_face_vertex4.x, back_face_vertex4.y, back_face_vertex1.x, back_face_vertex1.y, color)
         end
 
-
         -- Connecting lines
         if back_face_vertex1 ~= nil and back_face_vertex2 ~= nil then
             draw.line(front_face_vertex1.x, front_face_vertex1.y, back_face_vertex1.x, back_face_vertex1.y, color)
@@ -92,7 +91,7 @@ end
 
 -- activateHitArea(via.GameObject, chainsaw.collision.GimmickSensorUserData, chainsaw.InteractManager.WorkIndex, chainsaw.InteractTrigger.TargetType, System.Collections.Generic.IEnumerable`1<chainsaw.InteractTriggerActivated>)
 local function on_pre_interact_trigger_set_activate(args)
-    -- args[5] should be of type "chainsaw.InteractTrigger.TargetType"
+    -- args[6] should be of type "chainsaw.InteractTrigger.TargetType"
     -- 0 means TargetType.Pl00, probably player character?
     last_trigger_target_type = sdk.to_int64(args[6])
 
@@ -120,11 +119,10 @@ local function on_pre_interact_trigger_set_activate(args)
 
         local trigger_bounding_box = owner_game_object_collider:call("get_BoundingAabb()")
 
-        -- trigger_bounding_box_lower_corner_point = trigger_bounding_box:call("getCenter()")
-        -- trigger_bounding_box_upper_corner_point = trigger_bounding_box:call("getCenter()")
+        -- local trigger_bounding_box_center = trigger_bounding_box:call("getCenter()")
 
-        trigger_bounding_box_lower_corner_point = trigger_bounding_box.minpos -- + dummy_transform_origin
-        trigger_bounding_box_upper_corner_point = trigger_bounding_box.maxpos -- + dummy_transform_origin
+        trigger_bounding_box_lower_corner_point = trigger_bounding_box.minpos
+        trigger_bounding_box_upper_corner_point = trigger_bounding_box.maxpos
 
         if trigger_bounding_box_lower_corner_point == nil or trigger_bounding_box_upper_corner_point == nil then
             error("Failed to get trigger_bounding_box_lower_corner_point or trigger_bounding_box_upper_corner_point")
@@ -137,18 +135,10 @@ local function on_pre_interact_trigger_set_activate(args)
 end
 
 local function on_post_interact_trigger_set_activate(ret)
-    -- last_trigger_target_type = sdk.to_int64(ret)
-    
-    -- if (last_trigger_target_type == 1) then
-    --     area_hit_count = area_hit_count + 1
-    -- end
-
     return ret
 end
 
 -- sdk.hook(sdk.find_type_definition("chainsaw.InteractTriggerActivated"):get_method("set_Activate(chainsaw.InteractTriggerActivated.ActivateType)"),
-
-
 -- sdk.hook(sdk.find_type_definition("chainsaw.InteractTriggerAreaHit"):get_method("get_Type()"),
 sdk.hook(sdk.find_type_definition("chainsaw.InteractManager"):get_method("activateHitArea(via.GameObject, chainsaw.collision.GimmickSensorUserData, chainsaw.InteractManager.WorkIndex, chainsaw.InteractTrigger.TargetType, System.Collections.Generic.IEnumerable`1<chainsaw.InteractTriggerActivated>)"),
     on_pre_interact_trigger_set_activate,
@@ -171,81 +161,11 @@ re.on_frame(function()
 
             if v1 ~= nil and v2 ~= nil then
                 draw.line(v1.x, v1.y, v2.x, v2.y, 0xffffffff)
-
                 draw_wireframe_box(trigger_bounding_box_lower_corner_point, trigger_bounding_box_upper_corner_point, 0xff0000ff)
-
-                -- draw_wireframe_box(trigger_bounding_box_lower_corner_point, trigger_bounding_box_upper_corner_point)
-
-                -- Move to separate function
-                -- local v1a = draw.world_to_screen(trigger_bounding_box_lower_corner_point)
-                -- local v2b = draw.world_to_screen(trigger_bounding_box_upper_corner_point)
-            
-                -- local lower = trigger_bounding_box_lower_corner_point
-                -- local upper = trigger_bounding_box_upper_corner_point
-
-                -- local front_face_vertex1 = draw.world_to_screen(Vector3f.new(lower.x, lower.y, lower.z))
-                -- local front_face_vertex2 = draw.world_to_screen(Vector3f.new(lower.x, upper.y, lower.z))
-                -- local front_face_vertex3 = draw.world_to_screen(Vector3f.new(upper.x, upper.y, lower.z))
-                -- local front_face_vertex4 = draw.world_to_screen(Vector3f.new(upper.x, lower.y, lower.z))
-
-                -- local back_face_vertex1 = draw.world_to_screen(Vector3f.new(lower.x, lower.y, upper.z))
-                -- local back_face_vertex2 = draw.world_to_screen(Vector3f.new(lower.x, upper.y, upper.z))
-                -- local back_face_vertex3 = draw.world_to_screen(Vector3f.new(upper.x, upper.y, upper.z))
-                -- local back_face_vertex4 = draw.world_to_screen(Vector3f.new(upper.x, lower.y, upper.z))
-
-                --     -- draw.line(v1a.x, v1a.y, v1a.x, v2b.y, 0xff0000ff)
-                --     -- draw.line(v1a.x, v2b.y, v2b.x, v2b.y, 0xff0000ff)
-                --     -- draw.line(v2b.x, v2b.y, v2b.x, v1a.y, 0xff0000ff)
-                --     -- draw.line(v2b.x, v1a.y, v1a.x, v1a.y, 0xff0000ff)
-
-                -- -- Front face
-                -- if front_face_vertex1 ~= nil and front_face_vertex2 ~= nil then
-                --     draw.line(front_face_vertex1.x, front_face_vertex1.y, front_face_vertex2.x, front_face_vertex2.y, 0xff0000ff)
-                -- end
-                -- if front_face_vertex2 ~= nil and front_face_vertex3 ~= nil then
-                --     draw.line(front_face_vertex2.x, front_face_vertex2.y, front_face_vertex3.x, front_face_vertex3.y, 0xff0000ff)
-                -- end
-                -- if front_face_vertex3 ~= nil and front_face_vertex4 ~= nil then
-                --     draw.line(front_face_vertex3.x, front_face_vertex3.y, front_face_vertex4.x, front_face_vertex4.y, 0xff0000ff)
-                -- end
-                -- if front_face_vertex4 ~= nil and front_face_vertex1 ~= nil then
-                --     draw.line(front_face_vertex4.x, front_face_vertex4.y, front_face_vertex1.x, front_face_vertex1.y, 0xff0000ff)
-                -- end
-
-                -- -- Back face
-                -- if back_face_vertex1 ~= nil and back_face_vertex2 ~= nil then
-                --     draw.line(back_face_vertex1.x, back_face_vertex1.y, back_face_vertex2.x, back_face_vertex2.y, 0xff0000ff)
-                -- end
-                -- if back_face_vertex2 ~= nil and back_face_vertex3 ~= nil then
-                --     draw.line(back_face_vertex2.x, back_face_vertex2.y, back_face_vertex3.x, back_face_vertex3.y, 0xff0000ff)
-                -- end
-                -- if back_face_vertex3 ~= nil and back_face_vertex4 ~= nil then
-                --     draw.line(back_face_vertex3.x, back_face_vertex3.y, back_face_vertex4.x, back_face_vertex4.y, 0xff0000ff)
-                -- end
-                -- if back_face_vertex4 ~= nil and back_face_vertex1 ~= nil then
-                --     draw.line(back_face_vertex4.x, back_face_vertex4.y, back_face_vertex1.x, back_face_vertex1.y, 0xff0000ff)
-                -- end
-
-
-                -- -- Connecting lines
-                -- if back_face_vertex1 ~= nil and back_face_vertex2 ~= nil then
-                --     draw.line(front_face_vertex1.x, front_face_vertex1.y, back_face_vertex1.x, back_face_vertex1.y, 0xff0000ff)
-                -- end
-                -- if back_face_vertex2 ~= nil and back_face_vertex3 ~= nil then
-                --     draw.line(front_face_vertex2.x, front_face_vertex2.y, back_face_vertex2.x, back_face_vertex2.y, 0xff0000ff)
-                -- end
-                -- if back_face_vertex3 ~= nil and back_face_vertex4 ~= nil then
-                --     draw.line(front_face_vertex3.x, front_face_vertex3.y, back_face_vertex3.x, back_face_vertex3.y, 0xff0000ff)
-                -- end
-                -- if back_face_vertex4 ~= nil and back_face_vertex1 ~= nil then
-                --     draw.line(front_face_vertex4.x, front_face_vertex4.y, back_face_vertex4.x, back_face_vertex4.y, 0xff0000ff)
-                -- end
-                -- /Move to separate function
             end
         end
-
+        
         draw.world_text("TRIGGER", dummy_transform_origin, 0xffffffff)
-
         draw.world_text("+", trigger_bounding_box_lower_corner_point, 0xffffffff)
         draw.world_text("+", trigger_bounding_box_upper_corner_point, 0xffffffff)
     end
